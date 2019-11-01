7a1fd4de561fa34e3a6c9eaa0d72e455e02f1d0a
Bala <bala_b@hotmail.com>
2019-11-01 10:27:34 -0500

    initial draft

---
+++ notebooks/Users/babal@microsoft.com/SCM/deltaupdate.scala
@@ -0,0 +1,24 @@
+// Databricks notebook source
+
+
+// COMMAND ----------
+
+val dftrain = spark.read.option("header","true").option("inferschema" , "true").csv("/mnt/taxidata/train.csv")
+
+// COMMAND ----------
+
+dftrain.createOrReplaceTempView("updates")
+
+// COMMAND ----------
+
+// MAGIC %sql
+// MAGIC 
+// MAGIC MERGE INTO taxidata_delta
+// MAGIC using updates 
+// MAGIC ON taxidata_delta.pickup_longitude = updates.pickup_longitude AND taxidata_delta.pickup_latitude = updates.pickup_latitude
+// MAGIC  AND taxidata_delta.dropoff_longitude = updates.dropoff_longitude AND taxidata_delta.dropoff_latitude = updates.dropoff_latitude
+// MAGIC  AND taxidata_delta.passenger_count = updates.passenger_count 
+// MAGIC WHEN MATCHED THEN
+// MAGIC  UPDATE *
+// MAGIC WHEN NOT MATCHED 
+// MAGIC  THEN INSERT *
