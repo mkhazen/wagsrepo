fdb0f496384873bdda67dab2f0f527ac1cd2c234
Bala <bala_b@hotmail.com>
2019-12-10 19:45:34 -0600

    Update: DevOps PipelineMgmt.md README.md SQLQuery_1.sql dataorch filecountcheck.md images notebooks

---
+++ notebooks/Users/babal@microsoft.com/AML/amltest.py
@@ -0,0 +1,462 @@
+# Databricks notebook source
+
+import pyspark 
+print(print(pyspark.__version__)) 
+
+# COMMAND ----------
+
+import azureml.core
+print(azureml.core.VERSION)
+
+# COMMAND ----------
+
+import os
+
+subscription_id = "c46a9435-c957-4e6c-a0f4-b9a597984773"
+resource_group = "eastus_automl_rg"
+workspace_name = "eastus_automl_ws"
+workspace_region = "East US 2"
+
+# COMMAND ----------
+
+from azureml.core import Workspace
+
+try:
+    ws = Workspace(subscription_id = subscription_id, resource_group = resource_group, workspace_name = workspace_name)
+    # write the details of the workspace to a configuration file to the notebook library
+    ws.write_config()
+    print("Workspace configuration succeeded. Skip the workspace creation steps below")
+except:
+    print("Workspace not accessible. Change your parameters or create a new workspace below")
+
+# COMMAND ----------
+
+import azureml.core
+from azureml.core import Workspace, Datastore
+
+ws = Workspace.from_config()
+
+# COMMAND ----------
+
+from azureml.core.compute import ComputeTarget, AmlCompute
+from azureml.core.compute_target import ComputeTargetException
+
+# Choose a name for your CPU cluster
+cpu_cluster_name = "cpucluster"
+
+# Verify that cluster does not exist already
+try:
+    cpu_cluster = ComputeTarget(workspace=ws, name=cpu_cluster_name)
+    print("Found existing cpucluster")
+except ComputeTargetException:
+    print("Creating new cpucluster")
+    
+    # Specify the configuration for the new cluster
+    compute_config = AmlCompute.provisioning_configuration(vm_size="STANDARD_D2_V2",
+                                                           min_nodes=0,
+                                                           max_nodes=4)
+
+    # Create the cluster with the specified name and configuration
+    cpu_cluster = ComputeTarget.create(ws, cpu_cluster_name, compute_config)
+    
+    # Wait for the cluster to complete, show the output log
+    cpu_cluster.wait_for_completion(show_output=True)
+
+# COMMAND ----------
+
+import azureml.dataprep as dprep
+
+# COMMAND ----------
+
+dataset_root = "https://dprepdata.blob.core.windows.net/demo"
+
+green_path = "/".join([dataset_root, "green-small/*"])
+yellow_path = "/".join([dataset_root, "yellow-small/*"])
+
+green_df = dprep.read_csv(path=green_path, header=dprep.PromoteHeadersMode.GROUPED)
+# auto_read_file will automatically identify and parse the file type, and is useful if you don't know the file type
+yellow_df = dprep.auto_read_file(path=yellow_path)
+
+green_df.head(5)
+yellow_df.head(5)
+
+# COMMAND ----------
+
+all_columns = dprep.ColumnSelector(term=".*", use_regex=True)
+drop_if_all_null = [all_columns, dprep.ColumnRelationship(dprep.ColumnRelationship.ALL)]
+useful_columns = [
+    "cost", "distance", "dropoff_datetime", "dropoff_latitude", "dropoff_longitude",
+    "passengers", "pickup_datetime", "pickup_latitude", "pickup_longitude", "store_forward", "vendor"
+]
+
+# COMMAND ----------
+
+tmp_df = (green_df
+    .replace_na(columns=all_columns)
+    .drop_nulls(*drop_if_all_null)
+    .rename_columns(column_pairs={
+        "VendorID": "vendor",
+        "lpep_pickup_datetime": "pickup_datetime",
+        "Lpep_dropoff_datetime": "dropoff_datetime",
+        "lpep_dropoff_datetime": "dropoff_datetime",
+        "Store_and_fwd_flag": "store_forward",
+        "store_and_fwd_flag": "store_forward",
+        "Pickup_longitude": "pickup_longitude",
+        "Pickup_latitude": "pickup_latitude",
+        "Dropoff_longitude": "dropoff_longitude",
+        "Dropoff_latitude": "dropoff_latitude",
+        "Passenger_count": "passengers",
+        "Fare_amount": "cost",
+        "Trip_distance": "distance"
+     })
+    .keep_columns(columns=useful_columns))
+tmp_df.head(5)
+
+# COMMAND ----------
+
+green_df = tmp_df
+
+# COMMAND ----------
+
+tmp_df = (yellow_df
+    .replace_na(columns=all_columns)
+    .drop_nulls(*drop_if_all_null)
+    .rename_columns(column_pairs={
+        "vendor_name": "vendor",
+        "VendorID": "vendor",
+        "vendor_id": "vendor",
+        "Trip_Pickup_DateTime": "pickup_datetime",
+        "tpep_pickup_datetime": "pickup_datetime",
+        "Trip_Dropoff_DateTime": "dropoff_datetime",
+        "tpep_dropoff_datetime": "dropoff_datetime",
+        "store_and_forward": "store_forward",
+        "store_and_fwd_flag": "store_forward",
+        "Start_Lon": "pickup_longitude",
+        "Start_Lat": "pickup_latitude",
+        "End_Lon": "dropoff_longitude",
+        "End_Lat": "dropoff_latitude",
+        "Passenger_Count": "passengers",
+        "passenger_count": "passengers",
+        "Fare_Amt": "cost",
+        "fare_amount": "cost",
+        "Trip_Distance": "distance",
+        "trip_distance": "distance"
+    })
+    .keep_columns(columns=useful_columns))
+tmp_df.head(5)
+
+# COMMAND ----------
+
+yellow_df = tmp_df
+combined_df = green_df.append_rows([yellow_df])
+
+# COMMAND ----------
+
+decimal_type = dprep.TypeConverter(data_type=dprep.FieldType.DECIMAL)
+combined_df = combined_df.set_column_types(type_conversions={
+    "pickup_longitude": decimal_type,
+    "pickup_latitude": decimal_type,
+    "dropoff_longitude": decimal_type,
+    "dropoff_latitude": decimal_type
+})
+combined_df.keep_columns(columns=[
+    "pickup_longitude", "pickup_latitude", 
+    "dropoff_longitude", "dropoff_latitude"
+]).get_profile()
+
+# COMMAND ----------
+
+tmp_df = (combined_df
+    .drop_nulls(
+        columns=["pickup_longitude", "pickup_latitude", "dropoff_longitude", "dropoff_latitude"],
+        column_relationship=dprep.ColumnRelationship(dprep.ColumnRelationship.ANY)
+    ) 
+    .filter(dprep.f_and(
+        dprep.col("pickup_longitude") <= -73.72,
+        dprep.col("pickup_longitude") >= -74.09,
+        dprep.col("pickup_latitude") <= 40.88,
+        dprep.col("pickup_latitude") >= 40.53,
+        dprep.col("dropoff_longitude") <= -73.72,
+        dprep.col("dropoff_longitude") >= -74.09,
+        dprep.col("dropoff_latitude") <= 40.88,
+        dprep.col("dropoff_latitude") >= 40.53
+    )))
+tmp_df.keep_columns(columns=[
+    "pickup_longitude", "pickup_latitude", 
+    "dropoff_longitude", "dropoff_latitude"
+]).get_profile()
+
+# COMMAND ----------
+
+combined_df = tmp_df
+
+# COMMAND ----------
+
+combined_df.keep_columns(columns='store_forward').get_profile()
+
+# COMMAND ----------
+
+combined_df = combined_df.replace(columns="store_forward", find="0", replace_with="N").fill_nulls("store_forward", "N")
+
+# COMMAND ----------
+
+combined_df = combined_df.replace(columns="distance", find=".00", replace_with=0).fill_nulls("distance", 0)
+combined_df = combined_df.to_number(["distance"])
+
+# COMMAND ----------
+
+tmp_df_renamed = (tmp_df
+    .rename_columns(column_pairs={
+        "pickup_datetime_1": "pickup_date",
+        "pickup_datetime_2": "pickup_time",
+        "dropoff_datetime_1": "dropoff_date",
+        "dropoff_datetime_2": "dropoff_time"
+    }))
+tmp_df_renamed.head(5)
+
+# COMMAND ----------
+
+combined_df = tmp_df_renamed
+combined_df.get_profile()
+
+# COMMAND ----------
+
+tmp_df = (combined_df
+    .derive_column_by_example(
+        source_columns="pickup_date", 
+        new_column_name="pickup_weekday", 
+        example_data=[("2009-01-04", "Sunday"), ("2013-08-22", "Thursday")]
+    )
+    .derive_column_by_example(
+        source_columns="dropoff_date",
+        new_column_name="dropoff_weekday",
+        example_data=[("2013-08-22", "Thursday"), ("2013-11-03", "Sunday")]
+    )
+          
+    .split_column_by_example(source_column="pickup_time")
+    .split_column_by_example(source_column="dropoff_time")
+    # the following two split_column_by_example calls reference the generated column names from the above two calls
+    .split_column_by_example(source_column="pickup_time_1")
+    .split_column_by_example(source_column="dropoff_time_1")
+    .drop_columns(columns=[
+        "pickup_date", "pickup_time", "dropoff_date", "dropoff_time", 
+        "pickup_date_1", "dropoff_date_1", "pickup_time_1", "dropoff_time_1"
+    ])
+          
+    .rename_columns(column_pairs={
+        "pickup_date_2": "pickup_month",
+        "pickup_date_3": "pickup_monthday",
+        "pickup_time_1_1": "pickup_hour",
+        "pickup_time_1_2": "pickup_minute",
+        "pickup_time_2": "pickup_second",
+        "dropoff_date_2": "dropoff_month",
+        "dropoff_date_3": "dropoff_monthday",
+        "dropoff_time_1_1": "dropoff_hour",
+        "dropoff_time_1_2": "dropoff_minute",
+        "dropoff_time_2": "dropoff_second"
+    }))
+
+tmp_df.head(5)
+
+# COMMAND ----------
+
+tmp_df = tmp_df.drop_columns(columns=["pickup_datetime", "dropoff_datetime"])
+
+# COMMAND ----------
+
+type_infer = tmp_df.builders.set_column_types()
+type_infer.learn()
+type_infer
+
+# COMMAND ----------
+
+tmp_df = type_infer.to_dataflow()
+tmp_df.get_profile()
+
+# COMMAND ----------
+
+tmp_df = tmp_df.filter(dprep.col("distance") > 0)
+tmp_df = tmp_df.filter(dprep.col("cost") > 0)
+
+# COMMAND ----------
+
+import azureml.dataprep as dprep
+
+# COMMAND ----------
+
+import os
+file_path = os.path.join(os.getcwd(), "dflows.dprep")
+
+dflow_prepared = tmp_df
+package = dprep.Package([dflow_prepared])
+package.save(file_path)
+
+# COMMAND ----------
+
+import azureml.core
+import pandas as pd
+from azureml.core.workspace import Workspace
+import logging
+
+# COMMAND ----------
+
+ws = Workspace.from_config()
+# choose a name for the run history container in the workspace
+experiment_name = 'automated-ml-regression'
+# project folder
+project_folder = './automated-ml-regression'
+
+import os
+
+output = {}
+output['SDK version'] = azureml.core.VERSION
+output['Subscription ID'] = ws.subscription_id
+output['Workspace'] = ws.name
+output['Resource Group'] = ws.resource_group
+output['Location'] = ws.location
+output['Project Directory'] = project_folder
+pd.set_option('display.max_colwidth', -1)
+outputDf = pd.DataFrame(data = output, index = [''])
+outputDf.T
+
+# COMMAND ----------
+
+import azureml.dataprep as dprep
+
+file_path = os.path.join(os.getcwd(), "dflows.dprep")
+
+package_saved = dprep.Package.open(file_path)
+dflow_prepared = package_saved.dataflows[0]
+dflow_prepared.get_profile()
+
+# COMMAND ----------
+
+dflow_X = dflow_prepared.keep_columns(['pickup_weekday','pickup_hour', 'distance','passengers', 'vendor'])
+dflow_y = dflow_prepared.keep_columns('cost')
+
+# COMMAND ----------
+
+from sklearn.model_selection import train_test_split
+
+
+x_df = dflow_X.to_pandas_dataframe()
+y_df = dflow_y.to_pandas_dataframe()
+
+x_train, x_test, y_train, y_test = train_test_split(x_df, y_df, test_size=0.2, random_state=223)
+# flatten y_train to 1d array
+y_train.values.flatten()
+
+# COMMAND ----------
+
+automl_settings = {
+    "iteration_timeout_minutes" : 10,
+    "iterations" : 30,
+    "primary_metric" : 'spearman_correlation',
+    "preprocess" : True,
+    "verbosity" : logging.INFO,
+    "n_cross_validations": 5
+}
+
+# COMMAND ----------
+
+from azureml.train.automl import AutoMLConfig
+
+# local compute 
+automated_ml_config = AutoMLConfig(task = 'regression',
+                             debug_log = 'automated_ml_errors.log',
+                             path = project_folder,
+                             X = x_train.values,
+                             y = y_train.values.flatten(),
+                             **automl_settings)
+
+# COMMAND ----------
+
+from azureml.core.experiment import Experiment
+experiment=Experiment(ws, experiment_name)
+local_run = experiment.submit(automated_ml_config, show_output=True)
+
+# COMMAND ----------
+
+from azureml.widgets import RunDetails
+RunDetails(local_run).show()
+
+# COMMAND ----------
+
+children = list(local_run.get_children())
+metricslist = {}
+for run in children:
+    properties = run.get_properties()
+    metrics = {k: v for k, v in run.get_metrics().items() if isinstance(v, float)}
+    metricslist[int(properties['iteration'])] = metrics
+
+rundata = pd.DataFrame(metricslist).sort_index(1)
+rundata
+
+# COMMAND ----------
+
+best_run, fitted_model = local_run.get_output()
+print(best_run)
+print(fitted_model)
+
+# COMMAND ----------
+
+description = 'Automated Machine Learning Model'
+tags = None
+local_run.register_model(description=description, tags=tags)
+print(local_run.model_id) # Use this id to deploy the model as a web service in Azure
+
+# COMMAND ----------
+
+y_predict = fitted_model.predict(x_test.values) 
+print(y_predict[:10])
+
+# COMMAND ----------
+
+import matplotlib.pyplot as plt
+
+fig = plt.figure(figsize=(14, 10))
+ax1 = fig.add_subplot(111)
+
+distance_vals = [x[4] for x in x_test.values]
+y_actual = y_test.values.flatten().tolist()
+
+ax1.scatter(distance_vals[:100], y_predict[:100], s=18, c='b', marker="s", label='Predicted')
+ax1.scatter(distance_vals[:100], y_actual[:100], s=18, c='r', marker="o", label='Actual')
+
+ax1.set_xlabel('distance (mi)')
+ax1.set_title('Predicted and Actual Cost/Distance')
+ax1.set_ylabel('Cost ($)')
+
+plt.legend(loc='upper left', prop={'size': 12})
+plt.rcParams.update({'font.size': 14})
+plt.show()
+
+# COMMAND ----------
+
+from sklearn.metrics import mean_squared_error
+from math import sqrt
+
+rmse = sqrt(mean_squared_error(y_actual, y_predict))
+rmse
+
+# COMMAND ----------
+
+sum_actuals = sum_errors = 0
+
+for actual_val, predict_val in zip(y_actual, y_predict):
+    abs_error = actual_val - predict_val
+    if abs_error < 0:
+        abs_error = abs_error * -1
+    
+    sum_errors = sum_errors + abs_error
+    sum_actuals = sum_actuals + actual_val
+    
+mean_abs_percent_error = sum_errors / sum_actuals
+print("Model MAPE:")
+print(mean_abs_percent_error)
+print()
+print("Model Accuracy:")
+print(1 - mean_abs_percent_error)
+
+# COMMAND ----------
+
