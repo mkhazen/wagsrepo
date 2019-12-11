c7cf33e5d7745e393dc6e8747316c46968d7d80d
Bala <bala_b@hotmail.com>
2019-12-10 19:15:27 -0600

    Update: DevOps PipelineMgmt.md README.md SQLQuery_1.sql dataorch filecountcheck.md images notebooks

---
+++ notebooks/Users/babal@microsoft.com/SCM/amltest.py
@@ -0,0 +1,13 @@
+# Databricks notebook source
+import pyspark 
+print(print(pyspark.__version__))
+
+# COMMAND ----------
+
+#PyPi Name: **azureml-sdk[databricks]**
+#azureml-sdk[automl]
+
+# COMMAND ----------
+
+import azureml.core
+print(azureml.core.VERSION)
