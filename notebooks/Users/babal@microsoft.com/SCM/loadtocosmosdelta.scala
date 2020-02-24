766a47336b67069fa92d7c3baf778379bd313a8a
Bala <bala_b@hotmail.com>
2020-02-24 12:23:31 -0500

    Update: DevOps HyperscaleIDI.sql PipelineMgmt.md README.md SCMIDISQLDW.sql SQLQuery_1.sql SQLQuery_2.sql SQLQuery_3.sql SQLQueryopenhack.sql adfadb1.jpg adfadb2.jpg adfadb3.png dataorch dataset etlADFtest.md filecountcheck.md images notebooks

--- notebooks/Users/babal@microsoft.com/SCM/loadtocosmosdelta.scala
+++ notebooks/Users/babal@microsoft.com/SCM/loadtocosmosdelta.scala
@@ -13,3 +13,41 @@ display(custdatadf)
 // COMMAND ----------
 
 custdatadf.count
+
+// COMMAND ----------
+
+// Import Necessary Libraries
+import com.microsoft.azure.cosmosdb.spark.schema._
+import com.microsoft.azure.cosmosdb.spark._
+import com.microsoft.azure.cosmosdb.spark.config.Config
+
+// COMMAND ----------
+
+// Read Configuration
+val readConfig = Config(Map(
+  "Endpoint" -> "https://idicosmos.documents.azure.com:443/",
+  "Masterkey" -> "zoyDIf201Wpu6kCY0Njrh3R0v9SgUdYKmm3nvilyjYEXkhKv95aTgOBxN7tbSA4nU6iXPfcZh36v9rLL3EaEeg==",
+  "Database" -> "idiscm",
+  "Collection" -> "custdata",
+  "query_custom" -> "SELECT * from c" // Optional
+))
+
+// Connect via azure-cosmosdb-spark to create Spark DataFrame
+val itemdf = spark.read.cosmosDB(readConfig)
+itemdf.count()
+
+// COMMAND ----------
+
+// Write configuration
+
+val writeConfig = Config(Map(
+  "Endpoint" -> "https://idicosmos.documents.azure.com:443/",
+  "Masterkey" -> "zoyDIf201Wpu6kCY0Njrh3R0v9SgUdYKmm3nvilyjYEXkhKv95aTgOBxN7tbSA4nU6iXPfcZh36v9rLL3EaEeg==",
+  "Database" -> "idiscm",
+  "Collection" -> "custdata",
+  "WritingBatchSize" -> "5"
+))
+
+// Write to Cosmos DB from the flights DataFrame
+import org.apache.spark.sql.SaveMode
+custdatadf.write.mode(SaveMode.Overwrite).cosmosDB(writeConfig)
