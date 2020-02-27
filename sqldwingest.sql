a95197d91d44c27f5041a43eb145ba84fc21fa5d
bala_b@hotmail.com <bala_b@hotmail.com>
2020-02-27 12:49:33 -0600

    sql updates

---
+++ sqldwingest.sql
@@ -0,0 +1,45 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT *
+  FROM [dbo].[custdata1]
+
+  Use Master;
+
+  CREATE LOGIN adbingest WITH PASSWORD = 'Adb!2345678';
+
+  -- execute in the database
+  CREATE USER adbingest FROM LOGIN adbingest; 
+
+  ALTER ROLE dbmanager ADD MEMBER adbingest; 
+
+  GRANT ALTER ANY USER TO adbingest;
+
+  ALTER ROLE db_owner ADD MEMBER adbingest; 
+
+  --worked
+  EXEC sp_addrolemember 'db_owner', 'adbingest';
+
+-- https://docs.microsoft.com/en-us/azure/sql-data-warehouse/resource-classes-for-workload-management
+
+  SELECT name
+FROM   sys.database_principals
+WHERE  name LIKE '%rc%' AND type_desc = 'DATABASE_ROLE';
+
+EXEC sp_addrolemember 'xlargerc', 'adbingest';
+
+EXEC sp_droprolemember 'xlargerc', 'adbingest';
+
+CREATE WORKLOAD GROUP adbload 
+WITH
+  ( MIN_PERCENTAGE_RESOURCE = 50                -- integer value
+    , REQUEST_MIN_RESOURCE_GRANT_PERCENT = 25 -- factor of 26 (guaranteed a minimum of 8 concurrency)
+    , CAP_PERCENTAGE_RESOURCE = 100 )
+
+CREATE WORKLOAD CLASSIFIER adbstreamload WITH  
+( WORKLOAD_GROUP = 'adbserve'
+ ,MEMBERNAME     = 'adbingest'  
+ ,WLM_LABEL      = 'facts_loads' )
+
+SELECT COUNT(*) 
+  FROM custdata1
+  OPTION (LABEL = 'facts_loads')
+
